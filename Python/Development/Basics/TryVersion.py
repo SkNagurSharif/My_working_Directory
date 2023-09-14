fdd99d31fcf2de3b427669349f8a1f360d8113ce
Shaik Nagur Sharif <sknagursharif@outlook.com>
2023-09-14 21:59:16 +0530

    changes made

--- Python/Development/Basics/TryVersion.py
+++ Python/Development/Basics/TryVersion.py
@@ -1,8 +1,12 @@
-## Basic Syntax
+class Animal:
+    def __init__(self, species):
+        self.species = species
 
-class Dog:
-    def __init__(self):
-        self.breed = "Unknown"
+class Dog(Animal):
+    def __init__(self, breed):
+        super().__init__("Dog")
+        self.breed = breed
 
-dog = Dog()
-print(dog.breed)  # Output: Unknown
+dog = Dog("Labrador")
+print(dog.species)  # Output: Dog
+print(dog.breed)    # Output: Labrador
