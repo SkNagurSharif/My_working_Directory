f48f0d1e4948294c8abf3274754fe1e8013ed397
Shaik Nagur Sharif <sknagursharif@outlook.com>
2023-09-22 15:49:18 +0530

    project

--- GDJ2MR2/gdj2mr2_project/mvp_features/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/', views.register_view, name='register'),
-    path('add_equipment/', views.add_equipment, name='add_equipment'),
-    path('equipment_list/', views.equipment_list, name='equipment_list'),
-]
